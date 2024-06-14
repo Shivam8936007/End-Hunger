d6ad280b153c3b9a3896ed21d4e3bfa9e850e2dd
Shivam8936007 <shivam@tobor.in>
2024-06-14 14:48:42 +0530

    new ui

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer (){
+    return(
+        <div className="footer">
+
+            <h1>About Us</h1>
+          <h3>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</h3>
+        <p>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+
+        </div>
+    )
+}
---
+++ components/nav.tsx
@@ -0,0 +1,23 @@
+export default function Nav() {
+    return (
+      <nav className="navbar">
+      <div className="logo">
+        <h1>End Hunger</h1>
+      </div>
+      <ul className="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">How It Works</a></li>
+        <li><a href="#">Donate</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Gallery</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+
+         
+           
+   
